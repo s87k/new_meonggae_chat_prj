8d1f2a49b6f463eb66b5695a117500b06b869ef0
s87k <sd8273k@gmail.com>
2024-07-11 19:35:34 +0900

    240711 19:34 이미지 불러올 때 localhost 제거

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -157,7 +157,7 @@ $(function (){
 		} else if (img.startsWith('http')) {
 			imgSrc = img;
 		} else {
-			imgSrc = 'http://localhost/meonggae_prj/profile-img/' + img;
+			imgSrc = 'http://211.63.89.136//meonggae_prj/profile-img/' + img;
 		}
 		return imgSrc
 	} // getUserImg
